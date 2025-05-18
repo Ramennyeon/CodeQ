d1070b9540ac2079f65446184602c948a4c9fc5d
sirri <sirri@LAPTOP-JJ2FPVQD>
2025-05-18 12:44:48 +0800

    Nag lagay ng questions katamad ba

--- Code-Q/src/code/content/Questionnaire.java
+++ Code-Q/src/code/content/Questionnaire.java
@@ -47,6 +47,195 @@ public class Questionnaire {
                         ),
                     "easy"
                 )); 
+        
+                 
+                 easyQuestion.add(new Question( 
+                    "JVM stands for?",
+                    Arrays.asList(
+                        new Option("Java Virtual Machine", false),
+                        new Option("Java Verified Machine", true),
+                        new Option("Joint Virtual Machine", false),
+                        new Option("Java Version Module", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "Who is one of the creator of Java?",
+                    Arrays.asList(
+                        new Option("Elon Musk", false),
+                        new Option("James Gosling", true),
+                        new Option("Mark Zuckerberg", false),
+                        new Option("Dennis Richie", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What is the main feature of Java",
+                    Arrays.asList(
+                        new Option("Runs only on Java", false),
+                        new Option("Needs no Computer", true),
+                        new Option("Write once, write anywhere", false),
+                        new Option("Uses no Memory", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What Java keyword is used to print something to the console?",
+                    Arrays.asList(
+                        new Option("echo", false),
+                        new Option("print", true),
+                        new Option("output", false),
+                        new Option("System.out.println", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What is the extension of the compiled Java bytecode file?",
+                    Arrays.asList(
+                        new Option("java", false),
+                        new Option("exe", true),
+                        new Option("class", false),
+                        new Option("obj", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "Which Phase in PDLC include Bug fixing?",
+                    Arrays.asList(
+                        new Option("Planning", false),
+                        new Option("Implementation", true),
+                        new Option("Testing", false),
+                        new Option("Deployment", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What type of method is MAIN in java?",
+                    Arrays.asList(
+                        new Option("Private", false),
+                        new Option("static", true),
+                        new Option("Non-static", false),
+                        new Option("Abstract", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What does the keyword PUBLIC in Java mean?",
+                    Arrays.asList(
+                        new Option("Only the same class can access it", false),
+                        new Option("only subclasses can accses it", true),
+                        new Option("it is accessible from anywhere", false),
+                        new Option("it is private by default", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What does void mean in a method declaration?",
+                    Arrays.asList(
+                        new Option("The method returns a value", false),
+                        new Option("The method is static", true),
+                        new Option("The method returns nothing", false),
+                        new Option("The method is private", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What is the correct syntax to create a scanner object?",
+                    Arrays.asList(
+                        new Option("Scanner scan = System.in;", false),
+                        new Option("Scanner scan = new Scanner(System.in);", true),
+                        new Option("new Scanner = scan(System.in);", false),
+                        new Option("Scanner = new Scanner(scan);", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "Which of the following is NOT a Java access?",
+                    Arrays.asList(
+                        new Option("Protected", false),
+                        new Option("Open", true),
+                        new Option("Public", false),
+                        new Option("Private", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What is concatenation in Java?",
+                    Arrays.asList(
+                        new Option("Splitting strings", false),
+                        new Option("Combining numbers", true),
+                        new Option("sorting numbers", false),
+                        new Option("sorting strings", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "Which Keyword is used to iclude libraries in Java?",
+                    Arrays.asList(
+                        new Option("Include", false),
+                        new Option("using", true),
+                        new Option("import", false),
+                        new Option("require", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "Which method in Scanner reads integer input?",
+                    Arrays.asList(
+                        new Option("nextString()", false),
+                        new Option("nextLine()", true),
+                        new Option("nextInt()", false),
+                        new Option("nextInput()", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What symbol is used for escape squences?",
+                    Arrays.asList(
+                        new Option("/", false),
+                        new Option("]", true),
+                        new Option("!", false),
+                        new Option("%", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What is a Java class?",
+                    Arrays.asList(
+                        new Option("A file", false),
+                        new Option("A compiler", true),
+                        new Option("A blueprin for objects", false),
+                        new Option("A method", false)
+                        ),
+                    "easy"
+                )); 
+                 
+                 easyQuestion.add(new Question( 
+                    "What is the first line of a basic Java program called?",
+                    Arrays.asList(
+                        new Option("Method declaration", false),
+                        new Option("Class header", true),
+                        new Option("Package name", false),
+                        new Option("Function call", false)
+                        ),
+                    "easy"
+                )); 
+                
                  
                  mediumQuestion.add(new Question( 
                     "Which PDLC phase includes defining project scope, budget, and timeline?",
@@ -77,6 +266,192 @@ public class Questionnaire {
                         new Option("It defines the beginning of a method", true),
                         new Option("It declares a package", false),
                         new Option("It closes a method", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "What happens during the verification step in Java execution?",
+                    Arrays.asList(
+                        new Option("the program is compiled", false),
+                        new Option("Bytecode is translated to machine code", true),
+                        new Option("The code is checked for errors and security violations", false),
+                        new Option("The program is terminated", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "What type of Java method can be accessed without creating an object?",
+                    Arrays.asList(
+                        new Option("Abstract method", false),
+                        new Option("Instance method", true),
+                        new Option("Static method", false),
+                        new Option("Void method", false)
+                        ),
+                    "medium"
+                )); 
+              
+                 mediumQuestion.add(new Question( 
+                    "What kind of access modifier allows visibility only within the same class?",
+                    Arrays.asList(
+                        new Option("Public", false),
+                        new Option("Protected", true),
+                        new Option("Default", false),
+                        new Option("Private", false)
+                        ),
+                    "medium"
+                )); 
+              
+                 mediumQuestion.add(new Question( 
+                    "Which of these is NOT a valid Scanner method?",
+                    Arrays.asList(
+                        new Option("nextLine()", false),
+                        new Option("nextChar()", true),
+                        new Option("nextInt()", false),
+                        new Option("nextDouble()", false)
+                        ),
+                    "medium"
+                 ));         
+              
+                 mediumQuestion.add(new Question( 
+                    "Which package must be imported to use the scanner class?",
+                    Arrays.asList(
+                        new Option("java.lang", false),
+                        new Option("java.io", true),
+                        new Option("java.util", false),
+                        new Option("java.net", false)
+                        ),
+                    "medium"
+                )); 
+              
+                 mediumQuestion.add(new Question( 
+                    "In Java, what is the purpose of garbage collection?",
+                    Arrays.asList(
+                        new Option("Execute the program faster", false),
+                        new Option("Remove unused variables", true),
+                        new Option("Free up memory by removing unused objects", false),
+                        new Option("Delete temp files", false)
+                        ),
+                    "medium"
+                )); 
+              
+                 mediumQuestion.add(new Question( 
+                    "What escape sequence is used to move to a new line in java?",
+                    Arrays.asList(
+                        new Option("Java", false),
+                        new Option("/t", true),
+                        new Option("/r", false),
+                        new Option("/b", false)
+                        ),
+                    "medium"
+                )); 
+                 mediumQuestion.add(new Question( 
+                    "What is required for a method to be recognized as tha starting point of a Java application?",
+                    Arrays.asList(
+                        new Option("It must be named run", false),
+                        new Option("It must be a private method", true),
+                        new Option("It must be name main and be static", false),
+                        new Option("It must return an int", false)
+                        ),
+                    "medium"
+                ));
+                 
+                 mediumQuestion.add(new Question( 
+                    "Which of the following describes a CUSTOM CLASS in Java?" ,
+                    Arrays.asList(
+                        new Option("A class imported from a package", false),
+                        new Option("A user defined class tailored to specific needs", true),
+                        new Option("A default class from Java libraries", false),
+                        new Option("A read-only class", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "What will the output of this line be? System.out.println(Hello/tWorld);" ,
+                    Arrays.asList(
+                        new Option("Hello World", false),
+                        new Option("HelloWorld", true),
+                        new Option("Hello World", false),
+                        new Option("Hello/World", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "What does System.in represent in java" ,
+                    Arrays.asList(
+                        new Option("File input stream", false),
+                        new Option("Keyboard input stream", true),
+                        new Option("Output screen", false),
+                        new Option("File system", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "Which is a feature of Object-Oriented Programming" ,
+                    Arrays.asList(
+                        new Option("Compilation", false),
+                        new Option("Abstraction", true),
+                        new Option("Linking", false),
+                        new Option("Parsing", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "Which of the following is not part of Java's core programming paradigms? ?" ,
+                    Arrays.asList(
+                        new Option("Object-Oriented", false),
+                        new Option("Procedural", true),
+                        new Option("Functional", false),
+                        new Option("Imperative", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "Which of the following is true about the main method's argument?" ,
+                    Arrays.asList(
+                        new Option("It must always be int[] args", false),
+                        new Option("It holds command-line-arguments as Strings", true),
+                        new Option("It is afloat array", false),
+                        new Option("it is unused and unnecessary", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "How do you define a package in Java?" ,
+                    Arrays.asList(
+                        new Option("package = java.util", false),
+                        new Option("import package", true),
+                        new Option("package java.util", false),
+                        new Option("new package();", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "What is the output of the following? System.out.println(5+2=5+2);" ,
+                    Arrays.asList(
+                        new Option("5+2=7", false),
+                        new Option("5+2=52", true),
+                        new Option("7", false),
+                        new Option("Error", false)
+                        ),
+                    "medium"
+                )); 
+                 
+                 mediumQuestion.add(new Question( 
+                    "Which Keyword us used to define a method that doesn't return any value?" ,
+                    Arrays.asList(
+                        new Option("static", false),
+                        new Option("null", true),
+                        new Option("empty", false),
+                        new Option("void", false)
                         ),
                     "medium"
                 )); 
